629416e53b2725376e5884f83355d4d1b21826fa
Nadizdom34 <ndomingu@macalester.edu>
2025-12-06 14:21:28 -0600

    creating function to find closest centroid
    In order to run djikstras, I created a function to find the nearest centroids to the start and end point for robot

--- PolygonBreakdown.py
+++ PolygonBreakdown.py
@@ -158,3 +158,26 @@ axes.set_xlim(0,newImage.shape[1])
 axes.set_ylim(0,newImage.shape[0])
 plt.savefig("img_holes_cent2.png")
 plt.close("all")
+
+"""Finding the closest centroid to the point in the classroom environment"""
+def closestCent(centroids, point):
+    x,y = point
+    bestCent = None
+    bestDist - float('inf')
+
+    for coord in centroids:
+        d = ((x-coord[0])**2) + ((y-coord[1])**2)**0.5
+        if d < bestDist:
+            bestDist = d
+            bestCent = coord
+    return bestCent
+
+# Finding the closest centroid to the starting and end point for the robot
+startPoint = (0,0)
+endPoint = (newImage.shape[0],newImage.shape[1])
+
+startCent = closestCent(centroids, startPoint)
+endCent = closestCent(centroids, startPoint)
+
+# Use Djistras Algorithm on our Networkx Centroid Graph
+shortestPath = nx.dijkstra_path(centroid_Graph, startCent, endCent, weight = 'weight')
