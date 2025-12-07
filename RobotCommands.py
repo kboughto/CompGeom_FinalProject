b7b9d8077311f073fe5b438cbe4c1ed6bacf3bb4
kboughto <144381001+kboughto@users.noreply.github.com>
2025-12-07 15:39:37 -0600

    still working on robot file

--- RobotCommands.py
+++ RobotCommands.py
@@ -1,9 +1,40 @@
 import ConvexHullTakeTwo as Conv
 import PolygonBreakdown as Poly
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, MoveTank
+from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.sensor import IN1
+import math
 
 img = Conv.getImage()
 img_contours, img_hier = Conv.prepImage(img)
 hulls = Conv.makeConvexHulls(img_contours, img_hier, img)
 
 shortestCoords = Poly.getShortestPathCoords(hulls, img)
-print(shortestCoords)
+
+robot = MoveTank(OUTPUT_B, OUTPUT_C)
+gyro = GyroSensor(IN1) # sets up gyro: sensor that records angle rotated
+gyro.reset()
+
+driveSpeed = 50
+turnSpeed = 15
+
+"""
+Make following functions:
+- Turns robot
+- Drives robot
+- Normalizes angles
+"""
+
+classroomWidth = 607
+classroomLength = 396
+classroomWidthInFeet = 35.166666666
+classroomLengthInFeet = 23.25
+scalingFactor = ((classroomWidthInFeet/classroomWidth) + (classroomLengthInFeet/classroomLength))/2
+
+def computeDistance(currCoord, nextCoord):
+    distx = nextCoord[0] - currCoord[0]
+    disty = nextCoord[1] - currCoord[1]
+    dist = math.sqrt(distx**2 + disty**2) * scalingFactor
+    return dist
+
+
